33f43a0096c9035dbba6852979722823e9ac8a29
souleymane1234 <konatesouleymane42@gmail.com>
2025-03-11 10:38:05 +0000

    test ok

--- component/section-pages/section-2.jsx
+++ component/section-pages/section-2.jsx
@@ -27,7 +27,7 @@ const Section = () => {
     event.preventDefault(); // Empêche le rechargement de la page
 
     try {
-      const response = await fetch("http://localhost:3000/client", {
+      const response = await fetch("https://api.annour-travel.com/client", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
